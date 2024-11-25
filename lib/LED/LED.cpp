df9a1cb4abce555bff5f25882ded5abb08301fe2
LabIFSC <alfen_ferreira@ifsc.edu.br>
2024-11-25 19:36:52 -0300

    add LED integration sample lib

---
+++ lib/LED/LED.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include "LED.h"
+
+int contador = 0;
+unsigned long delayBotao;
+int estadoBotaoAnt = btNENHUM;
+
+LiquidCrystal lcd(pinRs, pinEn, pinD4, pinD5, pinD6, pinD7);
+//Para o exemplo de uso
+String descBotao[6] = {"", "Select", "Esquerda", "Abaixo", "Acima", "Direita"};
+
+void estadoBotao(int botao) {
+  //Quando um botao estiver apertado
+  if (botao != btNENHUM) {
+     Serial.println(botao);
+  }
+
+  //Quando o botao for apertado ou solto
+  if ((millis() - delayBotao) > tempoDebounce) {
+     if ((botao != btNENHUM) && (estadoBotaoAnt == btNENHUM) ) {
+        botaoApertado(botao); 
+        delayBotao = millis();
+     }
+
+     if ((botao == btNENHUM) && (estadoBotaoAnt != btNENHUM) ) {
+        botaoSolto(estadoBotaoAnt); 
+        delayBotao = millis();
+     }
+  }
+  estadoBotaoAnt = botao;
+}
+
+void botaoApertado(int botao) {
+  //Quando um botão for apertado
+
+  //Para o exemplo de uso
+  contador++;
+  lcd.clear();
+  lcd.print(descBotao[botao]);
+  lcd.setCursor(14,0);
+  lcd.print(contador);
+  lcd.setCursor(0,1);
+  lcd.print("apertado");
+}
+
+void botaoSolto(int botao) {
+  //Quando um botão for solto
+
+
+  //Para o exemplo de uso
+  lcd.clear();
+  lcd.print(descBotao[botao]);
+  lcd.setCursor(14,0);
+  lcd.print(contador);
+  lcd.setCursor(0,1);
+  lcd.print("solto");
+}
+
---
+++ lib/LED/LED.h
@@ -0,0 +1,33 @@
+#include <LiquidCrystal.h>
+
+//******* CONFIGURACAO DO PROJETO *********
+#define pinBotoes A0
+
+#define pinRs 8
+#define pinEn 9
+#define pinD4 4
+#define pinD5 5
+#define pinD6 6
+#define pinD7 7
+#define pinBackLight 10
+//*****************************************
+
+#define btNENHUM 0
+#define btSELECT 1
+#define btLEFT   2
+#define btUP     3
+#define btDOWN   4
+#define btRIGHT  5
+
+#define tempoDebounce 50
+
+
+
+void estadoBotao(int botao);
+void botaoApertado(int botao);
+void botaoSolto(int botao);
+
+extern int estadoBotaoAnt;
+
+extern LiquidCrystal lcd;
+extern String descBotao[6];
