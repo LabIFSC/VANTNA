3e2e6df985daeccb2b0483aa2b299e375ff40f01
Binboukami <luiz.butkeivicz@gmail.com>
2024-11-26 13:37:43 -0300

    add basic sample unit test for simarv

---
+++ test/test_simulation.cpp
@@ -0,0 +1,17 @@
+#include "unity.h"
+
+void assert_one_plus_one_equal_two()
+{
+  int a = 1; int b = 1;
+
+  int r = a + b;
+
+  TEST_ASSERT_EQUAL_CHAR(3, r);
+}
+
+int main( int argc, char **argv)
+{
+    UNITY_BEGIN();
+    RUN_TEST(assert_one_plus_one_equal_two);
+    UNITY_END();
+}
