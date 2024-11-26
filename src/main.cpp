99dd1b2a4f167de9ee9ac0fae27f7b584f1e73d1
Binboukami <luiz.butkeivicz@gmail.com>
2024-11-26 13:49:04 -0300

    linting and remove duplicated code ran for potentiometer test

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,30 @@
 #include <Arduino.h>
 #include <Motor.h>
 
-// Cria os objetos motor1 e motor2 utilizando o construtor Motor(pin1 , pin2, enable)
-/** In1 | In2 | EnableA */
-Motor M1(7,6,3);
-/** In3 | In4 | EnableB */
-Motor M2(5,4,2);
+#define HBRIDGE_5V A1
 
 void test_motor_w_potentiometer();
 
+// Cria os objetos motor1 e motor2 utilizando o construtor Motor(pin1 , pin2, enable)
+Motor M1(PIN7, PIN6, PIN3); /** In1 | In2 | EnableA */
+Motor M2(PIN5, PIN4, PIN2); /** In3 | In4 | EnableB */
+
 void setup() 
 {
-  // put your setup code here, to run once:
   M1.setup();
   M2.setup();
-  pinMode(A0,INPUT);
 
-  pinMode(A7, INPUT);
-  pinMode(A6, INPUT);
+  digitalWrite(HBRIDGE_5V, HIGH); // Tentativa de mandar 5v para ponte H pelo Arduino
+  // digitalWrite(HBRIDGE_12V, HIGH); // ???
+
+  // Init pins for potentiometer test
+  pinMode(A0, INPUT);
   pinMode(A1, INPUT);
 }
 
 void loop() 
 {
-  // test_motor_w_potentiometer();
-
   test_motor_w_potentiometer();
-
-  digitalWrite(A1, HIGH); // Ligar a ponte H
-
-  // test
-
-  long speed_m1 = 255;
-  long speed_m2 = -255;
-
-  if((speed_m1>=-10)&&(speed_m1<=10))
-    M1.freia();
-    else{
-  if(speed_m1<0)
-      M1.gira_antihorario(-speed_m1);
-  else
-      M1.gira_horario(speed_m1);
-    }
-
-    if((speed_m2>=-10)&&(speed_m2<=10))
-    M2.freia();
-    else{
-  if(speed_m2<0)
-      M2.gira_antihorario(-speed_m2);
-  else
-      M2.gira_horario(speed_m2);
-    }
-  // M2.gira_antihorario(speed_m2);
 
   delay(1000);
 }
@@ -61,6 +34,7 @@ void test_motor_w_potentiometer()
 
   long speed_m1 = map(analogRead(A0), 0, 1023, -255, 255);
   long speed_m2 = map(analogRead(A1), 0, 1023, -255, 255);
+
   if((speed_m1>=-10)&&(speed_m1<=10))
     M1.freia();
     else{
