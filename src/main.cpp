9e62de2103e6a31da84b5a4c05922c48e77c2ea3
LabIFSC <alfen.ferreira@ifsc.edu.br>
2024-11-27 11:32:33 -0300

    remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -13,42 +13,9 @@ void setup()
 {
   M1.setup();
   M2.setup();
-
-  digitalWrite(HBRIDGE_5V, HIGH); // Tentativa de mandar 5v para ponte H pelo Arduino
-  // digitalWrite(HBRIDGE_12V, HIGH); // ???
-
-  // Init pins for potentiometer test
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
 }
 
 void loop() 
 {
   test_motor_w_potentiometer();
-}
-
-void test_motor_w_potentiometer()
-{
-
-  long speed_m1 = map(analogRead(A0), 0, 1023, -255, 255);
-  long speed_m2 = map(analogRead(A1), 0, 1023, -255, 255);
-
-  if((speed_m1>=-10)&&(speed_m1<=10))
-    M1.freia();
-    else{
-  if(speed_m1<0)
-      M1.gira_antihorario(-speed_m1);
-  else
-      M1.gira_horario(speed_m1);
-    }
-
-    if((speed_m2>=-10)&&(speed_m2<=10))
-    M2.freia();
-    else{
-  if(speed_m2<0)
-      M2.gira_antihorario(-speed_m2);
-  else
-      M2.gira_horario(speed_m2);
-    }
-  // M2.gira_antihorario(speed_m2);
 }
