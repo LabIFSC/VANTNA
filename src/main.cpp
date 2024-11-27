722e9fbf1158f89e72ca25688752c0f7d88a1a90
LabIFSC <alfen.ferreira@ifsc.edu.br>
2024-11-27 11:36:41 -0300

    add todos

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
-#include <Arduino.h>
+ #include <Arduino.h>
 #include <Motor.h>
 
 #define HBRIDGE_5V A1
-
-void test_motor_w_potentiometer();
 
 // Cria os objetos motor1 e motor2 utilizando o construtor Motor(pin1 , pin2, enable)
 Motor M1(PIN7, PIN6, PIN3); /** In1 | In2 | EnableA */
@@ -13,9 +11,18 @@ void setup()
 {
   M1.setup();
   M2.setup();
+
+  // TODO: Inicializar periféricos
+  // - Bússola
+  // - GPS
+  // - Display LCD (IO)
+  // - Teclado (IO)
+
+  // TODO: Inicilizar GUI do painel de LCD
 }
 
 void loop() 
 {
-  test_motor_w_potentiometer();
+  // TODO: Ouvir por chamadas através dos botões para funções especificadas
+  // callbacks[buttons] = { [MENU_A] = enviar_coordenadas(...) }
 }
