a196a407a77fa744d3118bb349916d6b228f1534
LabIFSC <alfen_ferreira@ifsc.edu.br>
2024-11-28 16:26:10 -0300

    adiciona boilerplate de teste lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <Arduino.h>
 #include <Motor.h>
+#include "LiquidCrystal.h"
 
 #define HBRIDGE_5V A1
 
 // Cria os objetos motor1 e motor2 utilizando o construtor Motor(pin1 , pin2, enable)
 Motor M1(PIN7, PIN6, PIN3); /** In1 | In2 | EnableA */
 Motor M2(PIN5, PIN4, PIN2); /** In3 | In4 | EnableB */
+
+LiquidCrystal lcd_test;
+
+#define LCD_WIDTH 16
+#define LCD_HEIGHT 2
+
+void InitLCD()
+{
+  lcd_test.begin(LCD_WIDTH, LCD_HEIGHT);
+  lcd_test.clear();
+  lcd_test.setCursor(0, 0);
+}
 
 void setup() 
 {
