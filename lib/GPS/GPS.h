120dae942529243c0d2c07579c13aab7e9b431e7
LabIFSC <alfen_ferreira@ifsc.edu.br>
2024-11-29 17:18:09 -0300

    adiciona implementação inicial GPS

--- lib/GPS/GPS.h
+++ lib/GPS/GPS.h
@@ -1,3 +1,69 @@
 /**
  * Modelo: GY-NE06MV2
  */
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
+#define SS_RX_PIN 32
+#define SS_TX_PIN 33
+
+TinyGPS gps;
+// SoftwareSerial ss(SS_RX_PIN, SS_TX_PIN); // Using Serial 1 instead
+
+class GPS
+{
+public:
+    void setUp()
+    {
+        Serial.begin(9600);
+        Serial1.begin(4800);
+    }
+
+    void loop()
+    {
+        bool newData = false;
+        unsigned long chars;
+        unsigned short sentences, failed;
+
+        // For one second we parse GPS data and report some key values
+        for (unsigned long start = millis(); millis() - start < 1000;)
+        {
+            while (Serial1.available())
+            {
+                char c = Serial1.read();
+                // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+                if (gps.encode(c)) // Did a new valid sentence come in?
+                    newData = true;
+            }
+        }
+
+        // Teste num of satellites
+        // Serial.print(" SAT =");
+        // Serial.println(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+
+        if (newData) // false
+        {
+            float flat, flon;
+            unsigned long age;
+            gps.f_get_position(&flat, &flon, &age);
+            Serial.print("LAT=");
+            Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+            Serial.print(" LON=");
+            Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+            Serial.print(" SAT=");
+            Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+            Serial.print(" PREC=");
+            Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+        }
+
+        gps.stats(&chars, &sentences, &failed);
+        Serial.print(" CHARS=");
+        Serial.print(chars);
+        Serial.print(" SENTENCES=");
+        Serial.print(sentences);
+        Serial.print(" CSUM ERR=");
+        Serial.println(failed);
+        if (chars == 0)
+            Serial.println("** No characters received from GPS: check wiring **");
+        }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "LiquidCrystal.h"
 #include "Bussola.h"
 #include "Led.h"
+#include "GPS.h"
 
 #define HBRIDGE_5V A1
 
@@ -12,6 +13,8 @@
 Motor M1(PIN7, PIN6, PIN3); /** In1 | In2 | EnableA */
 Motor M2(PIN5, PIN4, PIN2); /** In3 | In4 | EnableB */
 Bussola B;
+
+GPS GPS_A;
 
 // LiquidCrystal lcd_test;
 
@@ -30,6 +33,9 @@ void setup()
   M1.setup();
   M2.setup();
   B.setUp();
+
+  GPS_A.setUp();
+
 
   // TODO: Inicializar periféricos
   // - Bússola
@@ -51,6 +57,8 @@ void loop()
   lcd.setCursor(1,0);
   lcd.print(a);
   delay(1000);
+
+  GPS_A.loop();
 
   // TODO: Ouvir por chamadas através dos botões para funções especificadas
   // callbacks[buttons] = { [MENU_A] = enviar_coordenadas(...) }
