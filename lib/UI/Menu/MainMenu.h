104db53e8a8edb7295eb8ef2c87d6dc4d75e73c5
LabIFSC <alfen_ferreira@ifsc.edu.br>
2024-12-05 17:09:29 -0300

    fix: caractere não limpando

--- lib/UI/Menu/MainMenu.h
+++ lib/UI/Menu/MainMenu.h
@@ -8,21 +8,23 @@ class MainMenu : public Menu
   public:
     MainMenu() : Menu(MID_MAIN)
     {
-      opcoes[0] = { "Executar", ExecMission };
-      opcoes[1] = { "Configurar", OpenSettings };
-      opcoes[2] = { "Dispositivo", NOT_IMPLEMENTED };
+      opcoes[0] = { "Executar       ", ExecMission };
+      opcoes[1] = { "Configurar     ", OpenSettings };
+      opcoes[2] = { "Dispositivo    ", NOT_IMPLEMENTED };
 
       this->n_opts = 3;
     }
 
     static void ExecMission(void)
     {
-      // TODO
+      //
     };
 
     static void OpenSettings(void)
     {
-      //
+      // Abrir novo menu
+      // UI.SetManuAtual(Config)
+      // ......
     };
 
     static void NOT_IMPLEMENTED(void) {};
