3c183f81ad7ccd71f52d361a33dcb9dc2070a769
LabIFSC <alfen_ferreira@ifsc.edu.br>
2024-12-05 13:55:43 -0300

    atualiza DisplayDevice com funcao custom de refresh rate

--- lib/UI/Display.h
+++ lib/UI/Display.h
@@ -1,15 +1,27 @@
 #pragma once
 #include "WString.h"
+#include "wiring_private.h"
 #include "LiquidCrystal.h"
 
 class DisplayDevice
 {
     LiquidCrystal* driver;
+    unsigned long default_refresh_rate = 1000;
 
     public:
         virtual LiquidCrystal* GetRawDevice();
 
-        virtual void Print(const String&) = 0;
-        virtual void PrintLine(const String&) = 0;
-        virtual void Clear() = 0;
+        /**
+         * Clears the screen at specified `refresh_rate` interval in ms.
+         * Default: `1000ms`
+         */
+        void Refresh(unsigned long ms = 1000)
+        {
+            unsigned long freq = default_refresh_rate;
+            if(ms != 1000)
+                freq = ms;
+
+            delay(freq);
+            driver->clear();
+        }
 };
